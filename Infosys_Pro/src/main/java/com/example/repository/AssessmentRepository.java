2098b7fea9fc959f152162fe2419f7ee2fef8867
Krishna <krishnashingan@gmail.com>
2025-04-01 08:09:52 +0530

    FIRST COMMIT

--- Infosys_Pro/src/main/java/com/example/repository/AssessmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.Model.Assessment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface AssessmentRepository extends MongoRepository<Assessment, String> {
-    List<Assessment> findByUsernameOrderByTimestampDesc(String username);
-}
--- Infosys_Pro/src/main/java/com/example/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.Model.Location;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LocationRepository extends MongoRepository<Location, String> {
-}
